a31baf5d211a3cf09db145429bf591eae856889b
sean <midruss@yahoo.com>
2022-03-15 13:41:38 -0400

    adjusted ports

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.get("/", (req, res) => res.send("Hello Mars"));
 app.use("/api/users", users);
 app.use("/api/posts", posts);
 
-const port = process.env.PORT || 5500; // change this back to 5000?
+const port = process.env.PORT || 5000; // change this back to 5000?
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
