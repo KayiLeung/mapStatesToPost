179d17d16ee8141fb5317e7aa7f62214f9eb47a7
sean <midruss@yahoo.com>
2022-03-16 16:20:23 -0400

    add validations and routew for comments, tested successfully on postman

--- models/Comment.js
+++ models/Comment.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-  user: {
+  author: {
     type: Schema.Types.ObjectId,
     ref: 'users'
   },
