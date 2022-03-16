db22a60b8ec55947d27828932b5fd9c0fafb9a01
Ka Yi Leung <email@email.com87kleung@gmail.comgit config user.email>
2022-03-15 23:09:29 -0700

    work with s3

--- services/photo_upload.js
+++ services/photo_upload.js
@@ -1,21 +1,21 @@
 const AWS = require("aws-sdk");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
-const keys = require('../config/keys');
 const s3 = new AWS.S3({
-    accessKeyId: keys.AWS.accessKeyId,
-    secretAccessKey: keys.AWS.secretAccessKey,
+    
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
 });
 const path = require('path')
 
 
+debugger
 const upload = multer({
-    fileFilter,
     storage: multerS3({
         s3: s3,
         //access control for the file
         acl: 'public-read',
-        bucket: keys.AWS.bucket,
+        bucket: process.env.AWS_BUCKET_NAME,
         contentType: multerS3.AUTO_CONTENT_TYPE,
         metadata: function (req, file, cb) {
             cb(null, { fieldName: file.fieldname });
@@ -29,5 +29,8 @@ const upload = multer({
 
     }),
 });
+
+
+
 
 module.exports = upload;
