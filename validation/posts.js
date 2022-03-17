cb476e64a072e2356f178ab0312d76a9fc39e6ad
sean <midruss@yahoo.com>
2022-03-17 16:22:18 -0400

    added stateName field to posts

--- validation/posts.js
+++ validation/posts.js
@@ -5,6 +5,7 @@ module.exports = function validatePostInput(data) {
   let errors = {};
 
   data.caption = validText(data.caption) ? data.caption : '';
+  data.stateName = validText(data.stateName) ? data.stateName : '';
 
   if (!Validator.isLength(data.caption, { min: 5, max: 140 })) {
     errors.caption = 'Post must be between 5 and 140 characters';
@@ -12,6 +13,10 @@ module.exports = function validatePostInput(data) {
 
   if (Validator.isEmpty(data.caption)) {
     errors.caption = 'Caption field is required';
+  }
+
+  if (Validator.isEmpty(data.stateName)) {
+    errors.caption = 'State name is required';
   }
 
   return {
